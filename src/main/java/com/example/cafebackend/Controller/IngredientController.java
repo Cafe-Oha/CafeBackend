ac184272a863f3f0b56122b1d9e12a1ca068f233
Emie0038 <Emie0038>
2022-11-22 12:52:09 +0100

    additional functions

--- src/main/java/com/example/cafebackend/Controller/IngredientController.java
+++ src/main/java/com/example/cafebackend/Controller/IngredientController.java
@@ -1,4 +1,21 @@
 package com.example.cafebackend.Controller;
 
+import com.example.cafebackend.Repository.IngredientRepository;
+import com.example.cafebackend.Service.IngredientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class IngredientController {
+
+    private final IngredientRepository ingredientRepository;
+
+    @Autowired
+    public IngredientController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @GetMapping
+    public List<Ingredient> getAllIngredients()
 }
