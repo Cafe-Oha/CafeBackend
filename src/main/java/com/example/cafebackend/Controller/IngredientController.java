95c9742d8020ddbe1086e52c54e98382d7eaa1d6
Wanesia <wintmiller161@gmail.com>
2022-11-23 11:13:32 +0100

    changed endpoints and added ingredientprice controller

--- src/main/java/com/example/cafebackend/Controller/IngredientController.java
+++ src/main/java/com/example/cafebackend/Controller/IngredientController.java
@@ -17,11 +17,6 @@ public class IngredientController {
     @Autowired
     IngredientRepository ingredientRepository;
 
-    @GetMapping("/hi")
-    public String sayHi(){
-        return "hi";
-    }
-
     @GetMapping()
     public List<Ingredient> getIngredients(){
         return ingredientRepository.findAll();
@@ -32,22 +27,29 @@ public class IngredientController {
         return ingredientRepository.findById(id).orElseThrow(()-> new IllegalStateException("ingredient with id="+id+" not found"));
     }
 
-    @PostMapping("/add")
+    @PostMapping()
     public ResponseEntity<String> addIngredient(@RequestBody Ingredient ingredient){
         ingredientRepository.save(ingredient);
         return new ResponseEntity<>("added ingredient", HttpStatus.OK);
     }
 
-    @PutMapping("/edit/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<String> updateIngredient(@PathVariable("id") Long id, @RequestBody Ingredient ingredient){
         ingredient.setId(id);
         ingredientRepository.save(ingredient);
         return new ResponseEntity<>("updated ingredient", HttpStatus.OK);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteIngredient(@PathVariable("id") Long id){
         ingredientRepository.deleteById(id);
-        return new ResponseEntity<>("deleted ingredient", HttpStatus.OK);    }
+        return new ResponseEntity<>("deleted ingredient", HttpStatus.OK);
+    }
+    @DeleteMapping()
+    public ResponseEntity<String> deleteIngredients(){
+        ingredientRepository.deleteAll();
+        return new ResponseEntity<>("deleted all ingredient", HttpStatus.OK);
+    }
+
 
 }
