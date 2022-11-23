d7a8339a113c4895d6005f400d269aff11bc8839
Wanesia <wintmiller161@gmail.com>
2022-11-23 10:18:53 +0100

    put and delete

--- src/main/java/com/example/cafebackend/Controller/IngredientController.java
+++ src/main/java/com/example/cafebackend/Controller/IngredientController.java
@@ -5,14 +5,13 @@ import com.example.cafebackend.Repository.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping(value = "/ingredients")
 public class IngredientController {
 
     @Autowired
@@ -23,14 +22,32 @@ public class IngredientController {
         return "hi";
     }
 
-    @GetMapping("/ingredients")
+    @GetMapping()
     public List<Ingredient> getIngredients(){
         return ingredientRepository.findAll();
     }
 
-    @PostMapping("/ingredients/add")
+    @GetMapping("/{id}")
+    public Ingredient getIngredientById(@PathVariable("id") Long id){
+        return ingredientRepository.findById(id).orElseThrow(()-> new IllegalStateException("ingredient with id="+id+" not found"));
+    }
+
+    @PostMapping("/add")
     public ResponseEntity<String> addIngredient(@RequestBody Ingredient ingredient){
         ingredientRepository.save(ingredient);
         return new ResponseEntity<>("added ingredient", HttpStatus.OK);
     }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<String> updateIngredient(@PathVariable("id") Long id, @RequestBody Ingredient ingredient){
+        ingredient.setId(id);
+        ingredientRepository.save(ingredient);
+        return new ResponseEntity<>("updated ingredient", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteIngredient(@PathVariable("id") Long id){
+        ingredientRepository.deleteById(id);
+        return new ResponseEntity<>("deleted ingredient", HttpStatus.OK);    }
+
 }
