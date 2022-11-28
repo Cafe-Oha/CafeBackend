998effd3df5a784f9e9d900910a4652fdc0ab6be
Emie0038 <Emie0038>
2022-11-28 11:05:41 +0100

    Initial Data2

--- src/main/java/com/example/cafebackend/common/InitialData.java
+++ src/main/java/com/example/cafebackend/common/InitialData.java
@@ -1,4 +1,4 @@
-package com.example.cafebackend.common;
+package com.example.cafebackend.Common;
 
 import com.example.cafebackend.Entity.Ingredient;
 import com.example.cafebackend.Entity.IngredientPrice;
@@ -6,59 +6,68 @@ import com.example.cafebackend.Entity.User;
 import com.example.cafebackend.Repository.IngredientPriceRepository;
 import com.example.cafebackend.Repository.IngredientRepository;
 import com.example.cafebackend.Repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+import java.time.LocalDate;
 
-@Configuration
-public class InitialData {
 
-    @Autowired
+//@Component
+public class InitialData implements CommandLineRunner {
+
     IngredientRepository ingredientRepository;
 
-    @Autowired
     IngredientPriceRepository ingredientPriceRepository;
 
-    @Autowired
     UserRepository userRepository;
 
-    @PostConstruct
-    //comment or uncomment annotation if you want it to create those things
-    public void initializeData(){
 
-        //ingredients
-        Ingredient ingredient1 = new Ingredient("potato", 1.0,"kg");
-        ingredientRepository.save(ingredient1);
-        Ingredient ingredient2 = new Ingredient("carrot", 35.1,"kg");
-        ingredientRepository.save(ingredient2);
-        Ingredient ingredient3 = new Ingredient("cabbage", 21.0,"kg");
-        ingredientRepository.save(ingredient3);
-        Ingredient ingredient4 = new Ingredient("milk", 1.2,"l");
-        ingredientRepository.save(ingredient4);
-        Ingredient ingredient5 = new Ingredient("cacao", 1.5,"l");
-        ingredientRepository.save(ingredient5);
-        Ingredient ingredient6 = new Ingredient("banana", 1.9,"kg");
-        ingredientRepository.save(ingredient6);
-        Ingredient ingredient7 = new Ingredient("orange", 1.9,"kg");
-        ingredientRepository.save(ingredient7);
-        Ingredient ingredient8 = new Ingredient("eggplant", 1.9,"kg");
-        ingredientRepository.save(ingredient8);
-        Ingredient ingredient9 = new Ingredient("tomatoes", 1.9,"kg");
-        ingredientRepository.save(ingredient9);
-        Ingredient ingredient10 = new Ingredient("spinach", 1.9,"kg");
-        ingredientRepository.save(ingredient10);
-
-        //ingredient price(date not working)
-//        IngredientPrice ingredientprice1 = new IngredientPrice(123.0,11.1,"2022-12-20 01:00:00");
-//        ingredientPriceRepository.save(ingredientprice1);
+    public InitialData(IngredientRepository ingredientRepository, IngredientPriceRepository ingredientPriceRepository, UserRepository userRepository) {
+        this.ingredientRepository = ingredientRepository;
+        this.ingredientPriceRepository = ingredientPriceRepository;
+        this.userRepository = userRepository;
+    }
 
 
-        //users
-        User user1 = new User("admin","admin123","ADMIN");
-        userRepository.save(user1);
-        User user2 = new User("user","user123","USER");
-        userRepository.save(user2);
+    @Override
+    public void run(String... args) throws Exception {
+     Ingredient ingredient1 = new Ingredient("Abocado", "kg", 12.0);
+     Ingredient ingredient2 = new Ingredient("Rice", "kg", 100.0);
+     Ingredient ingredient3 = new Ingredient("Carrot", "kg", 18.0);
+     Ingredient ingredient4 = new Ingredient("Orange", "kg", 150.0);
+     Ingredient ingredient5 = new Ingredient("Spinach", "kg", 9.0);
+     Ingredient ingredient6 = new Ingredient("Bread", "pcs", 10.0);
+
+
+     ingredientRepository.save(ingredient1);
+     ingredientRepository.save(ingredient2);
+     ingredientRepository.save(ingredient3);
+     ingredientRepository.save(ingredient4);
+     ingredientRepository.save(ingredient5);
+     ingredientRepository.save(ingredient6);
+
+
+     User user1 = new User("Anjellie", "?", "Admin");
+     User user2 = new User("Sajeta", "password", "Staff");
+     User user3 = new User("Niraj", "niraj", "Admin");
+
+
+     userRepository.save(user1);
+     userRepository.save(user2);
+     userRepository.save(user3);
+
+     IngredientPrice ingredientPrice1 =new IngredientPrice(55.5, 10.5, LocalDate.of(2022,11,25));
+     IngredientPrice ingredientPrice2 =new IngredientPrice(10.5, 7.5, LocalDate.of(2022,11,22));
+     IngredientPrice ingredientPrice3 =new IngredientPrice(50.0, 17.5, LocalDate.of(2022,11,20));
+     IngredientPrice ingredientPrice4 =new IngredientPrice(15.5, 15.5, LocalDate.of(2022,11,21));
+     IngredientPrice ingredientPrice5 =new IngredientPrice(35.5, 10.0, LocalDate.of(2022,11,24));
+
+     ingredientPriceRepository.save(ingredientPrice1);
+     ingredientPriceRepository.save(ingredientPrice2);
+     ingredientPriceRepository.save(ingredientPrice3);
+     ingredientPriceRepository.save(ingredientPrice4);
+     ingredientPriceRepository.save(ingredientPrice5);
+
     }
 
 }
