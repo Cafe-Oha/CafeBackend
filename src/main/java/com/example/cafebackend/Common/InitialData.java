f0d765a53c3977a0c1b68c4bcdc9008b0d76cb6c
mada0245 <80597666+mada0245@users.noreply.github.com>
2022-12-08 11:06:30 +0100

    small updates
    new passwords and usernames
    make loggedIn available

--- src/main/java/com/example/cafebackend/Common/InitialData.java
+++ src/main/java/com/example/cafebackend/Common/InitialData.java
@@ -49,8 +49,8 @@ public class InitialData implements CommandLineRunner {
         ingredientRepository.save(ingredient6);
 
 
-        User user1 = new User("Anjellie", "?", "Admin");
-        User user2 = new User("Sajeta", "password", "Staff");
+        User user1 = new User("Anjellie", "pass", "admin",true);
+        User user2 = new User("Sajeta", "password", "staff",false);
 
 
         userRepository.save(user1);
